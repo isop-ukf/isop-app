0eb49c58084801543966fcf6cadc18bf0a367f3b
br0kenpixel <23280129+br0kenpixel@users.noreply.github.com>
2025-10-07 14:43:45 +0200

    initial

---
+++ frontend/nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+
+export default defineNuxtConfig({
+    // interné pre nuxt, nemeniť
+    compatibilityDate: '2025-07-15',
+
+    // debugovacie nástroje v prehliadači
+    devtools: { enabled: true },
+
+    // externé moduly
+    modules: [
+        '@nuxt/image' // na obrázky
+    ]
+});
