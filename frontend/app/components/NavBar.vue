b6c5d256c4f61daa955015a46ba599d951ce80a4
br0kenpixel <23280129+br0kenpixel@users.noreply.github.com>
2025-10-14 18:21:47 +0200

    feat: add basic `NavBar` component

--- frontend/app/components/NavBar.vue
+++ frontend/app/components/NavBar.vue
@@ -2,7 +2,13 @@
 
 <template>
     <div>
-        Component: NavBar
+        <v-app-bar color="rgb(46, 125, 50)" style="color: white" :elevation="2">
+            <v-btn variant="text" dense to="/">Domov</v-btn>
+            <v-btn variant="text" dense>Register</v-btn>
+            <v-btn variant="text" dense>Login</v-btn>
+
+            <v-spacer></v-spacer>
+        </v-app-bar>
     </div>
 </template>
 
