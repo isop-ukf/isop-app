5aedd25cfec69b108bd4ab5240722240602d7d69
br0kenpixel <23280129+br0kenpixel@users.noreply.github.com>
2025-10-14 18:25:41 +0200

    feat: add basic `PageCard` component

--- frontend/app/components/PageCard.vue
+++ frontend/app/components/PageCard.vue
@@ -1,9 +1,38 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+function basicPropValidator(value: string, _other_props: any) {
+    // zatiaľ stačí vedieť či obsah nie je prázdny reťazec
+    return value.trim().length > 0
+}
+</script>
 
 <template>
-    <div>
-        Component: PageCard
-    </div>
+    <v-card variant="outlined" :width="300" class="d-flex flex-column" style="margin: 20px; cursor: pointer" link
+        :to="link">
+        <v-card-title class="text-wrap"><strong>{{ title }}</strong></v-card-title>
+        <v-card-text>
+            {{ description }}
+        </v-card-text>
+    </v-card>
 </template>
 
-<style scoped></style>
+<script lang="ts">
+export default {
+    props: {
+        title: {
+            required: true,
+            type: String,
+            validator: basicPropValidator
+        },
+        description: {
+            required: true,
+            type: String,
+            validator: basicPropValidator
+        },
+        link: {
+            required: true,
+            type: String,
+            validator: basicPropValidator
+        }
+    }
+}
+</script>
