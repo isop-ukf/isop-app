ceb4a38f0d09b39d43616740fbd4cf564559bbc6
br0kenpixel <23280129+br0kenpixel@users.noreply.github.com>
2025-10-14 18:30:34 +0200

    feat: add basic index page layout

--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -1,9 +1,21 @@
-<script setup lang="ts"></script>
-
 <template>
-    <div>
-        Page: index
-    </div>
-</template>
+    <v-container fluid>
+        <v-card>
+            <v-card-text>
+                <v-row>
+                    <PageCard title="Informácie o odbornej praxi pre študentov"
+                        description="Všetky informácie o predmete Oborná prax, ako ho absolvovať, pravidlá a zmluvy."
+                        link="/" />
 
-<style scoped></style>
+                    <PageCard title="Informácie o odbornej praxi pre firmy"
+                        description="Všetky informácie pre zamestnávateľov o zmluvách a pravidlách absolvovania odbornej praxe."
+                        link="/" />
+
+                    <PageCard title="O aplikácii" description="Informácie o systéme ISOP." link="/about" />
+
+                    <PageCard title="Iné" description="Iný obsah." link="/" />
+                </v-row>
+            </v-card-text>
+        </v-card>
+    </v-container>
+</template>
