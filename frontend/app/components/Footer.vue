9c4796b113656897e2ea79744c448136baa3a2c8
br0kenpixel <23280129+br0kenpixel@users.noreply.github.com>
2025-10-14 18:22:29 +0200

    feat: add basic `Footer` component

--- frontend/app/components/Footer.vue
+++ frontend/app/components/Footer.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts"></script>
 
 <template>
-    <div>
-        Component: Footer
-    </div>
+    <v-footer>
+        <div style="display: flex; flex-direction: column;">
+            <div>(c) Fakulta prírodných vied a informatiky, UKF v Nitre</div>
+        </div>
+    </v-footer>
 </template>
 
 <style scoped></style>
