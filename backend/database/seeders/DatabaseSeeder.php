3c45c3f9c95ce539e20e9eb0df18e90ee687f749
br0kenpixel <23280129+br0kenpixel@users.noreply.github.com>
2025-10-20 21:50:34 +0200

    refactor: update `DatabaseSeeder` to create a default admin user with complete details

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -13,11 +13,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
+        // create a default admin user
         User::factory()->create([
             'name' => 'Test User',
+            'first_name' => 'Test',
+            'last_name' => 'User',
             'email' => 'test@example.com',
+            'phone' => '+421907444555',
+            'role' => 'ADMIN',
         ]);
     }
 }
