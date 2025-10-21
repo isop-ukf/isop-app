18b76523e40344116276c094f0eac559120dae17
br0kenpixel <23280129+br0kenpixel@users.noreply.github.com>
2025-10-21 14:13:14 +0200

    feat: add admin, company, and student dashboard pages with initial template structure

--- frontend/app/pages/dashboard/index.vue
+++ frontend/app/pages/dashboard/index.vue
@@ -1,3 +1,20 @@
+<script setup lang="ts">
+import { Role } from '~/types/role';
+import type { User } from '~/types/user';
+
+const user = useSanctumUser<User>();
+const router = useRouter();
+
+onMounted(() => {
+    if (user.value?.role === Role.ADMIN) {
+        router.push('/dashboard/admin');
+    } else if (user.value?.role === Role.STUDENT) {
+        router.push('/dashboard/student');
+    }
+});
+</script>
+
 <template>
-    <div></div>
+    <div>
+    </div>
 </template>
