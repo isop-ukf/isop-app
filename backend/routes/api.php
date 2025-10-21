97ab4b78855e07722f7910788b0b87c4bfa991b3
br0kenpixel <23280129+br0kenpixel@users.noreply.github.com>
2025-10-21 13:55:35 +0200

    feat: enhance user endpoint to include company and student data

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,18 @@
 <?php
 
+use App\Models\Company;
+use App\Models\StudentData;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+    $user = $request->user();
+
+    $company_data = Company::whereContact($user->id)->get();
+    $student_data = StudentData::whereUserId($user->id)->get();
+
+    $user->company_data = $company_data->isEmpty() ? null : $company_data;
+    $user->student_data = $student_data->isEmpty() ? null : $student_data;
+
+    return $user;
 });
