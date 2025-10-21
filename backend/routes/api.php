60a49431dc8dd93eb36aa6017f4274a2719086fe
br0kenpixel <23280129+br0kenpixel@users.noreply.github.com>
2025-10-21 14:04:37 +0200

    fix: retrieve first company and student data directly for user endpoint

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -8,11 +8,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     $user = $request->user();
 
-    $company_data = Company::whereContact($user->id)->get();
-    $student_data = StudentData::whereUserId($user->id)->get();
+    $company_data = Company::whereContact($user->id)->get()->first();
+    $student_data = StudentData::whereUserId($user->id)->get()->first();
 
-    $user->company_data = $company_data->isEmpty() ? null : $company_data;
-    $user->student_data = $student_data->isEmpty() ? null : $student_data;
+    $user->company_data = $company_data;
+    $user->student_data = $student_data;
 
     return $user;
 });
