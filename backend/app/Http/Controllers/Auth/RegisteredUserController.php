7959eec118edcb21963766db1de75e2352495f74
br0kenpixel <23280129+br0kenpixel@users.noreply.github.com>
2025-10-21 18:00:45 +0200

    feat: implement user registration email notification and add email templates

--- backend/app/Http/Controllers/Auth/RegisteredUserController.php
+++ backend/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Mail\UserRegistrationCompleted;
 use App\Models\Company;
 use App\Models\StudentData;
 use App\Models\User;
@@ -11,6 +12,7 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Mail;
 
 class RegisteredUserController extends Controller
 {
@@ -71,9 +73,8 @@ class RegisteredUserController extends Controller
             ]);
         }
 
+        Mail::to($user)->sendNow(new UserRegistrationCompleted($user->name, $password));
         event(new Registered($user));
-
-        Auth::login($user);
 
         return response()->noContent();
     }
---
+++ backend/resources/views/mail/registration/completed.blade.php
@@ -0,0 +1,16 @@
+@include("parts.header")
+<p>Vážená/ý {{ $name }},</p>
+<p>vaša registrácia do systému ISOP UKF prebehla úspešne!</p>
+<br />
+
+<p>Vaše heslo je: <em>{{ $password }}</em></p>
+
+<br />
+
+<p>Ďakujeme za vašu registráciu.</p>
+
+<br />
+
+<p>s pozdravom</p>
+<p>Systém ISOP UKF</p>
+@include("parts.footer")
