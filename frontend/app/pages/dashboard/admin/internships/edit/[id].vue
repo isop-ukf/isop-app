8b326a722d9fead2dc2ef0b844aa0354e24cc41a
Veronika Fehérvíziová <128744051+VeronikaFeherviziova@users.noreply.github.com>
2025-11-01 22:17:34 +0100

    refactor: remove unused ID from alerts in internship editor

--- frontend/app/pages/dashboard/admin/internships/edit/[id].vue
+++ frontend/app/pages/dashboard/admin/internships/edit/[id].vue
@@ -53,16 +53,16 @@ const { data, error, refresh } = await useSanctumFetch<Internship>(`/api/interns
 
             <!-- Čakajúca hláška -->
             <v-alert v-if="loading" density="compact" text="Prosím čakajte..." title="Spracovávam" type="info"
-                id="login-error-alert" class="mx-auto alert"></v-alert>
+                class="mx-auto alert"></v-alert>
 
             <!-- Chybová hláška -->
             <v-alert v-if="action_error !== null" density="compact" :text="action_error" title="Chyba" type="error"
-                id="login-error-alert" class="mx-auto alert"></v-alert>
+                class="mx-auto alert"></v-alert>
 
             <div v-else>
                 <!-- Chybová hláška -->
                 <v-alert v-if="error" density="compact" :text="error?.message" title="Chyba" type="error"
-                    id="login-error-alert" class="mx-auto alert"></v-alert>
+                    class="mx-auto alert"></v-alert>
 
                 <div v-else>
                     <div>
