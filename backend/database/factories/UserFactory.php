eb122c41ab1be5514412b53429d1b8f3d365cff6
Andrej <andrejkraslan8@gmail.com>
2025-11-04 17:26:31 +0100

    pridanie atributov pouzivatelovi pre aktivaciu uctu

--- backend/database/factories/UserFactory.php
+++ backend/database/factories/UserFactory.php
@@ -36,6 +36,8 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'active' => true,
+            'needs_password_change' => false,
         ];
     }
 
