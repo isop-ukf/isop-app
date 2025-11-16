45f54058d40b69260abe03337c2757f780bc6d1b
br0kenpixel <23280129+br0kenpixel@users.noreply.github.com>
2025-11-16 16:25:40 +0100

    refactor: move `triggerDownload` function to `utils`

--- frontend/app/components/InternshipDocumentViewer.vue
+++ frontend/app/components/InternshipDocumentViewer.vue
@@ -7,16 +7,6 @@ const props = defineProps<{
 
 const client = useSanctumClient();
 
-function triggerDownload(file: Blob, file_name: string) {
-    const url = window.URL.createObjectURL(file);
-    const link = document.createElement('a');
-    link.href = url;
-    link.download = `${file_name}.pdf`;
-    link.target = "_blank";
-    link.click();
-    window.URL.revokeObjectURL(url);
-}
-
 async function downloadAgreement() {
     const agreement: Blob = await client(`/api/internships/${props.internship.id}/agreement`);
     triggerDownload(agreement, `agreement-${props.internship.id}`);
