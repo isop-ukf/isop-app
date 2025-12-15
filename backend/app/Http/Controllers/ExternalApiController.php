c68aebc331af0f15363be6503d3cc588642344e6
Sofia Reháková <125893846+sofiarehakova@users.noreply.github.com>
2025-12-15 17:51:56 +0100

    fix: incorrect mail parameters in API call for external systems

--- backend/app/Http/Controllers/ExternalApiController.php
+++ backend/app/Http/Controllers/ExternalApiController.php
@@ -85,15 +85,26 @@ class ExternalApiController extends Controller
             'modified_by' => $user->id,
         ]);
 
+        // mail študentovi
         Mail::to($internship->student)
             ->sendNow(new InternshipStatusUpdated(
                 $internship,
-                $user->name,
-                $internship->student->name,
-                $internship->company->name,
                 $currentStatus,
-                $request->enum('status', InternshipStatus::class),
-                $request->note
+                $newStatus->status,
+                $request->note,
+                $user,
+                recipiantIsStudent: true,
+            ));
+
+        // mail firme
+        Mail::to($internship->company->contactPerson->email)
+            ->sendNow(new InternshipStatusUpdated(
+                $internship,
+                $currentStatus,
+                $newStatus->status,
+                $request->note,
+                $user,
+                recipiantIsStudent: false,
             ));
 
         $newStatus->save();
