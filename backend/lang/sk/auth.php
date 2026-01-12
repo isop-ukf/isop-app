81a2d67c09049d3520c9241de9edf876af65b885
dkecskes <steamacc311@gmail.com>
2026-01-12 20:11:58 +0100

    feat: add Slovak language localization for backend

---
+++ backend/lang/sk/auth.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Tieto prihlasovacie údaje sa nezhodujú s našimi záznamami.',
+    'password' => 'Zadané heslo je nesprávne.',
+    'throttle' => 'Príliš veľa pokusov o prihlásenie. Skúste to znova o :seconds sekúnd.',
+    'inactive_account' => 'Váš účet nie je aktívny. Kontaktujte, prosím, administrátora.',
+
+];
